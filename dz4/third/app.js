189db947a71d91e898f3f8526a412535484c6fca
Mila <119810982+millfsoo@users.noreply.github.com>
2023-08-23 06:42:50 +0600

    Update app.js

--- dz4/third/app.js
+++ dz4/third/app.js
@@ -1,8 +1,13 @@
-var array = ["name", "John", "lastname", "Black", "age", "23"];
-var totalObject = {};
-for (var i = 0; i < array.length; i += 2) {
-  var key = array[i];
-  var value = array[i + 1];
-  totalObject[key] = value;
-}
-console.log(totalObject);
+const buttons = document.querySelectorAll(".click");
+const outputs = document.querySelectorAll(".text");
+let previousText = "";
+
+buttons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    const inputText = prompt("Введите текст:");
+    if (inputText !== null) {
+      previousText = inputText;
+      outputs[index].textContent = previousText;
+    }
+  });
+});
