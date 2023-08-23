af8b85b0b75bdb3ca4449474791b707d213c28b8
Mila <119810982+millfsoo@users.noreply.github.com>
2023-08-23 06:37:42 +0600

    Update app.js

--- dz4/second/app.js
+++ dz4/second/app.js
@@ -1,11 +1,36 @@
-function averageCalculator() {
-  var sum = 0;
-  var count = arguments.length;
-  for (var i = 0; i < count; i++) {
-    sum += arguments[i];
-  }
-  var average = sum / count;
-  return average;
-}
-var total = averageCalculator(26, 11, 5, 19, 28, 4);
-console.log(total);
+const num1Input = document.getElementById("num1");
+const num2Input = document.getElementById("num2");
+const plusButton = document.getElementById("plus");
+const minusButton = document.getElementById("minus");
+const multiplicationButton = document.getElementById("multiplication");
+const divisionButton = document.getElementById("division");
+const calculateButton = document.getElementById("calculate");
+const resultDisplay = document.getElementById("total");
+
+plusButton.addEventListener("click", () => {
+  const num1 = parseFloat(num1Input.value);
+  const num2 = parseFloat(num2Input.value);
+  const result = num1 + num2;
+  resultDisplay.textContent = `Результат: ${result}`;
+});
+
+minusButton.addEventListener("click", () => {
+  const num1 = parseFloat(num1Input.value);
+  const num2 = parseFloat(num2Input.value);
+  const result = num1 - num2;
+  resultDisplay.textContent = `Результат: ${result}`;
+});
+
+multiplicationButton.addEventListener("click", () => {
+  const num1 = parseFloat(num1Input.value);
+  const num2 = parseFloat(num2Input.value);
+  const result = num1 * num2;
+  resultDisplay.textContent = `Результат: ${result}`;
+});
+
+divisionButton.addEventListener("click", () => {
+  const num1 = parseFloat(num1Input.value);
+  const num2 = parseFloat(num2Input.value);
+  const result = num1 / num2;
+  resultDisplay.textContent = `Результат: ${result}`;
+});
